60a57b390248d9f53e8ace0d7766e0fe1a830c12
Mafe Avendaño <mafe.ud@gmail.com>
2017-01-12 11:29:58 -0500

    Se agrega Distinct al modelo de asignaturas_elegibles

--- models/asignaturas_elegibles.go
+++ models/asignaturas_elegibles.go
@@ -51,7 +51,7 @@ func GetAsignaturasElegiblesById(id int) (v *AsignaturasElegibles, err error) {
 func GetAllAsignaturasElegibles(query map[string]string, fields []string, sortby []string, order []string,
 	offset int64, limit int64) (ml []interface{}, err error) {
 	o := orm.NewOrm()
-	qs := o.QueryTable(new(AsignaturasElegibles)).RelatedSel()
+	qs := o.QueryTable(new(AsignaturasElegibles)).RelatedSel().Distinct()
 	// query k=v
 	for k, v := range query {
 		// rewrite dot-notation to Object__Attribute
