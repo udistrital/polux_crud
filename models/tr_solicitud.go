b1eca8e3ed606fc89d5a9005ce6b4aafcc5f9264
JuanPiedrahita <juan.p15@hotmail.com>
2017-09-15 17:01:11 -0500

    Se modifica transaccion de la solicitud para registrar la respuesta de la solicitud, en este caso, EstadoSolicitud={Id:1 (radicada)}

--- models/tr_solicitud.go
+++ models/tr_solicitud.go
@@ -8,6 +8,7 @@ import (
 
 type TrSolicitud struct {
 	Solicitud         *SolicitudTrabajoGrado
+	Respuesta         *RespuestaSolicitud
 	DetallesSolicitud *[]DetalleSolicitud
 	UsuariosSolicitud *[]UsuarioSolicitud
 }
@@ -19,6 +20,12 @@ func AddTransaccionSolicitud(m *TrSolicitud) (alerta []string, err error) {
 	alerta = append(alerta, "Success")
 	if id, err := o.Insert(m.Solicitud); err == nil {
 		fmt.Println(id)
+		m.Respuesta.SolicitudTrabajoGrado.Id = int(id)
+		if _, err = o.Insert(m.Respuesta); err != nil {
+			err = o.Rollback()
+			alerta[0] = "Error"
+			alerta = append(alerta, "ERROR_SOLICITUDES_3")
+		}
 		for _, v := range *m.DetallesSolicitud {
 			v.SolicitudTrabajoGrado.Id = int(id)
 			if _, err = o.Insert(&v); err != nil {
