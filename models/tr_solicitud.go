55f0d248cebe8a9514e82de1d2bbb5d5fcf0438b
Mafe Avendaño <mafe.ud@gmail.com>
2017-10-05 12:57:36 -0500

    #16454# Cambios para prueba de comentarios a Tuleap

--- models/tr_solicitud.go
+++ models/tr_solicitud.go
@@ -22,13 +22,16 @@ func AddTransaccionSolicitud(m *TrSolicitud) (alerta []string, err error) {
 		fmt.Println(id)
 		m.Respuesta.SolicitudTrabajoGrado.Id = int(id)
 		if _, err = o.Insert(m.Respuesta); err != nil {
+			fmt.Println(err)
 			err = o.Rollback()
+
 			alerta[0] = "Error"
 			alerta = append(alerta, "ERROR_SOLICITUDES_3")
 		}
 		for _, v := range *m.DetallesSolicitud {
 			v.SolicitudTrabajoGrado.Id = int(id)
 			if _, err = o.Insert(&v); err != nil {
+				fmt.Println(err)
 				err = o.Rollback()
 				alerta[0] = "Error"
 				alerta = append(alerta, "ERROR_SOLICITUDES_1")
@@ -36,8 +39,10 @@ func AddTransaccionSolicitud(m *TrSolicitud) (alerta []string, err error) {
 		}
 
 		for _, j := range *m.UsuariosSolicitud {
+			fmt.Println(err)
 			j.SolicitudTrabajoGrado.Id = int(id)
 			if _, err = o.Insert(&j); err != nil {
+				fmt.Println(err)
 				err = o.Rollback()
 				alerta[0] = "Error"
 				alerta = append(alerta, "ERROR_SOLICITUDES_2")
@@ -45,6 +50,7 @@ func AddTransaccionSolicitud(m *TrSolicitud) (alerta []string, err error) {
 		}
 		err = o.Commit()
 	} else {
+		fmt.Println(err)
 		err = o.Rollback()
 	}
 	return
