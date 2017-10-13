a2bc0fcdd4315ffe5e465ba913d0857beb8d2385
Mafe Avendaño <mafe.ud@gmail.com>
2017-10-13 11:53:57 -0500

    #16978# Se agrega RelatedSel() a area_conocimiento y area_docente

--- models/area_conocimiento.go
+++ models/area_conocimiento.go
@@ -50,7 +50,7 @@ func GetAreaConocimientoById(id int) (v *AreaConocimiento, err error) {
 func GetAllAreaConocimiento(query map[string]string, fields []string, sortby []string, order []string,
 	offset int64, limit int64) (ml []interface{}, err error) {
 	o := orm.NewOrm()
-	qs := o.QueryTable(new(AreaConocimiento))
+	qs := o.QueryTable(new(AreaConocimiento)).RelatedSel()
 	// query k=v
 	for k, v := range query {
 		// rewrite dot-notation to Object__Attribute
