644668877e821c1de4ace1e0df5e58cd67c26af4
Mafe Avendaño <mafe.ud@gmail.com>
2017-10-18 10:10:23 -0500

    Agrega RelatedSel() a revisión y estado de la revisión

--- models/estado_revision_trabajo_grado.go
+++ models/estado_revision_trabajo_grado.go
@@ -49,7 +49,7 @@ func GetEstadoRevisionTrabajoGradoById(id int) (v *EstadoRevisionTrabajoGrado, e
 func GetAllEstadoRevisionTrabajoGrado(query map[string]string, fields []string, sortby []string, order []string,
 	offset int64, limit int64) (ml []interface{}, err error) {
 	o := orm.NewOrm()
-	qs := o.QueryTable(new(EstadoRevisionTrabajoGrado))
+	qs := o.QueryTable(new(EstadoRevisionTrabajoGrado)).RelatedSel()
 	// query k=v
 	for k, v := range query {
 		// rewrite dot-notation to Object__Attribute
--- models/revision_trabajo_grado.go
+++ models/revision_trabajo_grado.go
@@ -52,7 +52,7 @@ func GetRevisionTrabajoGradoById(id int) (v *RevisionTrabajoGrado, err error) {
 func GetAllRevisionTrabajoGrado(query map[string]string, fields []string, sortby []string, order []string,
 	offset int64, limit int64) (ml []interface{}, err error) {
 	o := orm.NewOrm()
-	qs := o.QueryTable(new(RevisionTrabajoGrado))
+	qs := o.QueryTable(new(RevisionTrabajoGrado)).RelatedSel()
 	// query k=v
 	for k, v := range query {
 		// rewrite dot-notation to Object__Attribute
