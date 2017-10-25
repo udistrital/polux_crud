b91ef0b456cbeb92dfdebf051e02de20f087bc61
Mafe Avendaño <mafe.ud@gmail.com>
2017-10-25 15:10:39 -0500

    #17277# Se modifica estructura TrRespuestaSolicitud de la transacción de la respuesta a una solicitud de trabajo de grado

--- models/tr_respuesta_solicitud.go
+++ models/tr_respuesta_solicitud.go
@@ -7,12 +7,8 @@ import (
 )
 
 type TrRespuestaSolicitud struct {
-	TrabajoGrado            *TrabajoGrado
-	EstudianteTrabajoGrado  *[]EstudianteTrabajoGrado
-	DocumentoEscrito        *DocumentoEscrito
-	DocumentoTrabajoGrado   *DocumentoTrabajoGrado
-	AreasTrabajoGrado       *[]AreasTrabajoGrado
-	VinculacionTrabajoGrado *[]VinculacionTrabajoGrado
+	RespuestaSolicitud *RespuestaSolicitud
+	DocumentoEscrito   *DocumentoEscrito
 }
 
 //funcion para la transaccion de solicitudes
@@ -20,81 +16,21 @@ func AddTransaccionRespuestaSolicitud(m *TrRespuestaSolicitud) (alerta []string,
 	o := orm.NewOrm()
 	o.Begin()
 	alerta = append(alerta, "Success")
-	if id, err := o.Insert(m.TrabajoGrado); err == nil {
+	if id, err := o.Insert(m.RespuestaSolicitud); err == nil {
 		fmt.Println(id)
-
-		for _, v := range *m.EstudianteTrabajoGrado {
-			v.TrabajoGrado.Id = int(id)
-			if _, err = o.Insert(&v); err != nil {
-				fmt.Println(err)
-				err = o.Rollback()
-				alerta[0] = "Error"
-				alerta = append(alerta, "ERROR_SOLICITUDES_1")
-			}
-		}
-
-		for _, v := range *m.AreasTrabajoGrado {
-			v.TrabajoGrado.Id = int(id)
-			if _, err = o.Insert(&v); err != nil {
-				fmt.Println(err)
-				err = o.Rollback()
-				alerta[0] = "Error"
-				alerta = append(alerta, "ERROR_SOLICITUDES_2")
-			}
-		}
-
-		for _, v := range *m.VinculacionTrabajoGrado {
-			v.TrabajoGrado.Id = int(id)
-			if _, err = o.Insert(&v); err != nil {
-				fmt.Println(err)
-				err = o.Rollback()
-				alerta[0] = "Error"
-				alerta = append(alerta, "ERROR_SOLICITUDES_3")
-			}
-		}
 
 		if id_documento, err := o.Insert(m.DocumentoEscrito); err == nil {
 			fmt.Println(id_documento)
-			m.DocumentoTrabajoGrado.TrabajoGrado.Id = int(id)
-			m.DocumentoTrabajoGrado.DocumentoEscrito.Id = int(id_documento)
+			/*
+				m.DocumentoTrabajoGrado.TrabajoGrado.Id = int(id)
+				m.DocumentoTrabajoGrado.DocumentoEscrito.Id = int(id_documento)
 
-			if id_documento, err := o.Insert(m.DocumentoTrabajoGrado); err == nil {
-				fmt.Println(id_documento)
-			}
+				if id_documento, err := o.Insert(m.DocumentoTrabajoGrado); err == nil {
+					fmt.Println(id_documento)
+				}*/
 		}
 
-		/*
-			m.Respuesta.SolicitudTrabajoGrado.Id = int(id)
-			if _, err = o.Insert(m.Respuesta); err != nil {
-				fmt.Println(err)
-				err = o.Rollback()
-
-				alerta[0] = "Error"
-				alerta = append(alerta, "ERROR_SOLICITUDES_3")
-			}
-			for _, v := range *m.DetallesSolicitud {
-				v.SolicitudTrabajoGrado.Id = int(id)
-				if _, err = o.Insert(&v); err != nil {
-					fmt.Println(err)
-					err = o.Rollback()
-					alerta[0] = "Error"
-					alerta = append(alerta, "ERROR_SOLICITUDES_1")
-				}
-			}
-
-			for _, j := range *m.UsuariosSolicitud {
-				fmt.Println(err)
-				j.SolicitudTrabajoGrado.Id = int(id)
-				if _, err = o.Insert(&j); err != nil {
-					fmt.Println(err)
-					err = o.Rollback()
-					alerta[0] = "Error"
-					alerta = append(alerta, "ERROR_SOLICITUDES_2")
-				}
-			}
-
-		*/
-		err = o.Commit()
+		//err = o.Commit()
 	} else {
 		fmt.Println(err)
 		err = o.Rollback()
