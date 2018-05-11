741c2ce000cf221610118ed93fd6b59492a02fa2
karianov <karianov@correo.udistrital.edu.co>
2018-05-11 13:03:11 -0500

    #22485# Adición de filtros para espacio académico inscrito, detalle solicitud

--- controllers/tr_registrar_materias_posgrado.go
+++ controllers/tr_registrar_materias_posgrado.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"fmt"
 	"encoding/json"
 
 	"github.com/udistrital/Polux_API_Crud/models"
@@ -25,14 +26,19 @@ func (c *TrRegistrarMateriasPosgrado) URLMapping() {
 // @router / [post]
 func (c *TrRegistrarMateriasPosgrado) Post() {
 	var v models.TrRegistrarMateriasPosgrado
+	fmt.Println("Before Unmarshal");
 	if err := json.Unmarshal(c.Ctx.Input.RequestBody, &v); err == nil {
+		fmt.Println("Yes Unmarshal");
 		if response, err := models.AddTransaccionRegistrarMateriasPosgrado(&v); err == nil {
+			fmt.Println("Yes model");
 			c.Ctx.Output.SetStatus(201)
 			c.Data["json"] = response
 		} else {
+			fmt.Println("Bad model");
 			c.Data["json"] = err.Error()
 		}
 	} else {
+		fmt.Println("Bad Unmarshal");
 		c.Data["json"] = err.Error()
 	}
 	c.ServeJSON()
