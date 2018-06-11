3089bf90baaa9c9ed9fac6230630041889c833e3
juanpiedrahita <juangonzalez1597@gmail.com>
2018-06-10 19:28:35 -0500

    #23448# Se agregan los detalles de la pasantia al consultar el trabajo de grado

--- routers/router.go
+++ routers/router.go
@@ -62,6 +62,12 @@ func init() {
 			),
 		),
 
+		beego.NSNamespace("/detalle_pasantia",
+			beego.NSInclude(
+				&controllers.DetallePasantiaController{},
+			),
+		),
+
 		beego.NSNamespace("/detalle_tipo_solicitud",
 			beego.NSInclude(
 				&controllers.DetalleTipoSolicitudController{},
