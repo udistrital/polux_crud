29988c056257781efd5524e28b10249c71eecbc1
karianov <karianov@correo.udistrital.edu.co>
2018-07-25 17:49:53 -0500

    #24260# Actualizar la transacción para subir la primera versión del documento

--- models/tr_actualizar_documento_tg.go
+++ models/tr_actualizar_documento_tg.go
@@ -21,25 +21,29 @@ func AddTransaccionActualizarDocumentoTg(m *TrActualizarDocumentoTg) (alerta []s
 	// Insert del documento escrito
 	if idDocumentoEscrito, err := o.Insert(m.DocumentoEscrito); err == nil {
 		fmt.Println("Written document inserted:", idDocumentoEscrito)
-		// Update del documento del trabajo de grado
-		m.DocumentoTrabajoGrado.DocumentoEscrito.Id = int(idDocumentoEscrito)
-		if num, err := o.Update(m.DocumentoTrabajoGrado, "DocumentoEscrito"); err == nil {
-			fmt.Println("Number of degree work documents updated:", num)
-			// Update del trabajo de grado
-			if num, err := o.Update(m.TrabajoGrado, "EstadoTrabajoGrado"); err == nil {
-				fmt.Println("Number of degree work records updated:", num)
-				err = o.Commit()
+		if(m.TrabajoGrado.EstadoTrabajoGrado.Id == 4 || m.TrabajoGrado.EstadoTrabajoGrado.Id == 15){
+			// Update del documento del trabajo de grado
+			m.DocumentoTrabajoGrado.DocumentoEscrito.Id = int(idDocumentoEscrito)
+			if num, err := o.Update(m.DocumentoTrabajoGrado, "DocumentoEscrito"); err == nil {
+				fmt.Println("Number of degree work documents updated:", num)
 			} else {
 				fmt.Println(err)
 				alerta[0] = "Error"
 				alerta = append(alerta, "ERROR_RTA_SOLICITUD_1")
 				err = o.Rollback()
 			}
-		} else {
-			fmt.Println(err)
-			alerta[0] = "Error"
-			alerta = append(alerta, "ERROR_RTA_SOLICITUD_1")
-			err = o.Rollback()
+		} else if(m.TrabajoGrado.EstadoTrabajoGrado.Id == 13 ){
+			m.DocumentoTrabajoGrado.Id = 0
+			m.DocumentoTrabajoGrado.DocumentoEscrito.Id = int(idDocumentoEscrito)
+			m.DocumentoTrabajoGrado.TrabajoGrado.Id = int(m.TrabajoGrado.Id)
+			if idDocumentoTg, err := o.Insert(m.DocumentoTrabajoGrado); err == nil {
+				fmt.Println("degree work document created:", idDocumentoTg)
+			} else {
+				fmt.Println(err)
+				alerta[0] = "Error"
+				alerta = append(alerta, "ERROR_RTA_SOLICITUD_1")
+				err = o.Rollback()
+			}
 		}
 	} else {
 		fmt.Println(err)
@@ -47,5 +51,16 @@ func AddTransaccionActualizarDocumentoTg(m *TrActualizarDocumentoTg) (alerta []s
 		alerta = append(alerta, "ERROR_RTA_SOLICITUD_2")
 		err = o.Rollback()
 	}
+	// Update del trabajo de grado
+	if num, err := o.Update(m.TrabajoGrado, "EstadoTrabajoGrado"); err == nil {
+		fmt.Println("Number of degree work records updated:", num)
+	} else {
+		fmt.Println(err)
+		alerta[0] = "Error"
+		alerta = append(alerta, "ERROR_RTA_SOLICITUD_1")
+		err = o.Rollback()
+	}
+
+	err = o.Commit()
 	return
 }
