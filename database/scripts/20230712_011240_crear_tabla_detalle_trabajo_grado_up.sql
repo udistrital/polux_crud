5fa41293cf6b1c83b4bc669325e62ff5acbf2c78
Mechaz123 <danielalejandrorojasgonzalez@gmail.com>
2023-07-12 18:06:11 -0500

    fix: Se corrige la consulta SQL para la creación de la tabla detalle_trabajo_grado y se realiza la corección para la columna de agregar objetivo, con respecto a las importaciones

--- database/scripts/20230712_011240_crear_tabla_detalle_trabajo_grado_up.sql
+++ database/scripts/20230712_011240_crear_tabla_detalle_trabajo_grado_up.sql
@@ -6,3 +6,9 @@ CREATE TABLE academica.detalle_trabajo_grado (
 	CONSTRAINT detalle_trabajo_grado_pk PRIMARY KEY (id),
 	CONSTRAINT trabajo_grado_fk FOREIGN KEY (trabajo_grado) REFERENCES academica.trabajo_grado (id)
 );
+
+COMMENT ON TABLE academica.detalle_trabajo_grado IS 'Tabla para almacenar los detalles del trabajo de grado de todas las modalidades';
+COMMENT ON COLUMN academica.detalle_trabajo_grado.id IS 'Identificador de la tabla detalle_trabajo_grado';
+COMMENT ON COLUMN academica.detalle_trabajo_grado.parametro IS 'parametro al cual se hará referencia, ya que guardará el id de la tabla parametro en la base de datos udistrital_core';
+COMMENT ON COLUMN academica.detalle_trabajo_grado.valor IS 'El valor que llevara dicho parametro';
+COMMENT ON COLUMN academica.detalle_trabajo_grado.trabajo_grado IS 'Llave foranea que hará referencia al trabajo de grado relacionado';
