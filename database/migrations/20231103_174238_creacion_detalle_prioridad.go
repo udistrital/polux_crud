a2303e33d2a44b3a4d63cd7c787bb621a6fed0eb
Diego Gutierrez <diagutierrezro@unal.edu.co>
2023-11-03 18:44:39 -0500

    fix: Migracion prioridad

--- database/migrations/20231103_174238_creacion_detalle_prioridad.go
+++ database/migrations/20231103_174238_creacion_detalle_prioridad.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
+	"strings"
+
 	"github.com/astaxie/beego/migration"
 )
 
@@ -20,11 +24,33 @@ func init() {
 // Run the migrations
 func (m *CreacionDetallePrioridad_20231103_174238) Up() {
 	// use m.SQL("CREATE TABLE ...") to make schema update
+	file, err := ioutil.ReadFile("../scripts/20231103_174238_creacion_detalle_prioridad_up.sql")
 
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	requests := strings.Split(string(file), ";\n")
+
+	for _, request := range requests {
+		fmt.Println(request)
+		m.SQL(request)
+	}
 }
 
 // Reverse the migrations
 func (m *CreacionDetallePrioridad_20231103_174238) Down() {
 	// use m.SQL("DROP TABLE ...") to reverse schema update
+	file, err := ioutil.ReadFile("../scripts/20231103_174238_creacion_detalle_prioridad_down.sql")
 
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	requests := strings.Split(string(file), ";\n")
+
+	for _, request := range requests {
+		fmt.Println(request)
+		m.SQL(request)
+	}
 }
