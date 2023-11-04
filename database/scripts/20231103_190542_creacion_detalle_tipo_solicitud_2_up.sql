1bc2e8e8587b9da547604ffdea2a58522df0e2f4
Diego Gutierrez <diagutierrezro@unal.edu.co>
2023-11-03 19:41:46 -0500

    feat: Migracion detalle tipo sol 2

---
+++ database/scripts/20231103_190542_creacion_detalle_tipo_solicitud_2_up.sql
@@ -0,0 +1,6 @@
+INSERT INTO academica.detalle_tipo_solicitud(detalle, modalidad_tipo_solicitud, activo, requerido, numero_orden)
+VALUES (81, 13, true, true, 3);
+
+UPDATE academica.detalle_tipo_solicitud
+SET numero_orden=4
+WHERE id=309;
