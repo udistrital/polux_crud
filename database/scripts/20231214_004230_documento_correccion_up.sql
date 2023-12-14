5fb8ca740b2184a712099433acab01f116955da6
lud <ludwinggsanchez@gmail.com>
2023-12-14 11:35:02 -0500

    feat udistrital/polux_cliente#440: Revision documentos v2
    - Se almacena enlace de documentos de correcciones existentes.

--- database/scripts/20231214_004230_documento_correccion_up.sql
+++ database/scripts/20231214_004230_documento_correccion_up.sql
@@ -3,3 +3,7 @@ ADD COLUMN IF NOT EXISTS enlace_documento VARCHAR(36) DEFAULT '';
 
 ALTER TABLE academica.correccion
 ALTER COLUMN enlace_documento DROP DEFAULT;
+
+UPDATE academica.correccion
+SET enlace_documento = observacion
+WHERE documento = TRUE;
